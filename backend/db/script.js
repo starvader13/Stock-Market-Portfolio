0d44baef6c79617a3f97940ec52c277ef81e1172
starvader13 <ashutoshgupta1311@gmail.com>
2024-05-30 19:11:10 +0530

    Setup database schmea and connection
    Signed-off-by: starvader13 <ashutoshgupta1311@gmail.com>

---
+++ backend/db/script.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const connectionString = process.env.DATABASE_URL;
+const databaseName = "stock-market-portfolio";
+
+mongoose.connect(`${connectionString}/${databaseName}`).then(()=>{
+    console.log("Database Connected Successfully");
+}).catch(()=>{
+    console.log("Database Connection Failed");
+});
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    password: String,
+});
+
+const stockSchema = new mongoose.Schema({
+    index: Number,
+    name: String,
+    description: String,
+    price: Number,
+    watchlist: {
+        type: Boolean,
+        default: false
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+const Stock = mongoose.model('Stock', stockSchema);
+
+module.exports = {
+    User,
+    Stock
+}
