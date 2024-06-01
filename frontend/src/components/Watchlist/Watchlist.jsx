3ad2fedbb070f8350a0ca8f6abcf30bc707026e1
starvader13 <ashutoshgupta1311@gmail.com>
2024-06-01 17:02:23 +0530

    Update Watchlist to render only when the user is signed in
    Signed-off-by: starvader13 <ashutoshgupta1311@gmail.com>

--- frontend/src/components/Watchlist/Watchlist.jsx
+++ frontend/src/components/Watchlist/Watchlist.jsx
@@ -1,7 +1,7 @@
 import Header from "../Header/Header.jsx";
 import StockComponent from "../Home/StockComponent.jsx";
 import {useRecoilValue, useSetRecoilState} from "recoil";
-import {allStocksAtom, updateAllStockStateAtom} from "../../store/atom.js";
+import {allStocksAtom, updateAllStockStateAtom, userSignedInAtom} from "../../store/atom.js";
 import {useEffect} from "react";
 import fetchAllStocks from "../../store/fetch.js";
 import("./watchlist.css")
@@ -11,6 +11,7 @@ export default function Watchlist(){
     const allStocks = useRecoilValue(allStocksAtom);
     const setAllStocks = useSetRecoilState(allStocksAtom);
     const updateAllStockState = useRecoilValue(updateAllStockStateAtom);
+    const signedIn = useRecoilValue(userSignedInAtom);
 
     useEffect(() => {
         fetchAllStocks(setAllStocks);
@@ -22,10 +23,11 @@ export default function Watchlist(){
         </div>
         <Header />
         {
+            signedIn ?
             allStocks.map((stock, index)=>{
                 index+=1;
                 return stock.watchlist ? <StockComponent key={stock._id} updateId={stock._id} index={index} name={stock.name} price={stock.price} symbol={stock.symbol} watchlist={stock.watchlist}/> : null
-            })
+            }) : null
         }
     </div>
 }
